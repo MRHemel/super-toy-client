029b05827426a3cce56ad4d29f0048e6de5b64de
MRHemel <hemelrahman80@gmail.com>
2023-06-09 07:34:35 +0600

    navbar added to the site

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import logo from '../../assets/images/super-toy-logo-removebg-preview.png'
+import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 const Header = () => {
+    const navItems = <>
+        <li><Link to={'/'}>Home</Link></li>
+
+        <li><Link>All Toys</Link></li>
+        <li><Link>My Toys</Link></li>
+        <li><Link>Add a Toy</Link></li>
+        <li><Link to={'/blog'}>Blogs</Link></li>
+        <li><Link to={'/login'}>Login</Link></li>
+    </>
     return (
-        <div>
-            <h2>this header</h2>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItems}
+
+                    </ul>
+                </div>
+                <Link to={'/'}><h1 className='text-5xl font-extrabold'>Super Toy</h1></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-2xl">
+                    {navItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <FaUser className='text-3xl'></FaUser>
+            </div>
         </div>
     );
 };
